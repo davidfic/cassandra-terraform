a09288eaff4560353ed17ccfc1066fcb3805089a
David Ficociello <davidfic@gmail.com>
2017-01-21 21:42:26 -0500

    made use of availability zone data source

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "az1-pub-sn" {
   vpc_id            = "${var.vpc_id}"
   cidr_block        = "${var.az1_pub_subnet_cidr}"
-  availability_zone = "${var.az1}"
+  availability_zone = "${join(",", data.aws_availability_zones.available.names)}"
   map_public_ip_on_launch = true
   tags {
     Name = "${var.environment}-pub-${var.az1}"
